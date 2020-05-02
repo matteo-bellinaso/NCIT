f3c3f2593e220b1b8852321ab27b23e4e6a6e101
matteo-bellinaso <matteo.bellinaso@gmail.com>
2020-05-02 17:58:08 +0200

    first view

---
+++ NCIT/Authetication.swift
@@ -0,0 +1,18 @@
+//
+//  Authetication.swift
+//  NCIT
+//
+//  Created by Matteo Bellinaso on 02/05/2020.
+//  Copyright © 2020 Matteo Bellinaso. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+class Autheticator {
+    
+    func autheticate() {
+        
+    }
+}
