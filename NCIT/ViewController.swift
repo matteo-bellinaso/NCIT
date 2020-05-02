de7c2dda79fc3b48da32c2c86543afc6dab18a8e
matteo-bellinaso <matteo.bellinaso@gmail.com>
2020-05-02 14:46:53 +0200

    Initial Commit

---
+++ NCIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCIT
+//
+//  Created by Matteo Bellinaso on 02/05/2020.
+//  Copyright © 2020 Matteo Bellinaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
